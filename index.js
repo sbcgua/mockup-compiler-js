e249b0eac872cd7ac08d83fb5809ce001d004516
Alexander Tsybulsky <atsybulsky@sbcg.com.ua>
2020-03-28 20:34:10 +0200

    quiet command param

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const App = require('./lib/app');
 const { readConfig } = require('./lib/config');
 const commander = require('commander');
 const chalk = require('chalk');
+const Logger = require('./lib/logger');
 
 const CONFIG_DEFAULT_PATH = './.mock-config.json';
 
@@ -19,14 +20,18 @@ function argsToConfig(args) {
         zipPath:   args.zip,
         includes:  args.include && [args.include],
         eol:       args.eol,
+        quiet:     args.quiet,
     };
-    for (let k of Object.keys(config)) if (!config[k]) delete config[k];
+    for (let k of Object.keys(config)) {
+        if (!config[k]) delete config[k];
+    }
     return config;
 }
 
 async function main(args) {
     const config = readConfig(args.config || CONFIG_DEFAULT_PATH, argsToConfig(args));
     if (!args.color) chalk.enabled = false;
+    config.logger = new Logger({quiet: config.quiet});
 
     try {
         const app = new App(config, args.watch);
@@ -39,6 +44,7 @@ async function main(args) {
 
 [
     ['-b, --no-color', 'suppress colors'],
+    ['-q, --quiet', 'show no output'],
     ['-w, --watch', 'keep watching the files and re-render when changed'],
     ['-c, --config <path>', 'read config from this file'],
     ['-s, --source <path>', 'source directory'],
@@ -50,7 +56,7 @@ async function main(args) {
 commander.parse(process.argv);
 
 process.on('unhandledRejection', (reason) => {
-    console.log('CRASH! unhandledRejection:', reason);
+    if (!commander.quiet) console.error('CRASH! unhandledRejection:', reason);
     process.exit(1);
 });
 main(commander.opts());
