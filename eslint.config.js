a92e0ef28263012ee99dd1094e9c6dc555f246f3
Alexander Tsybulsky <atsybulsky@sbcg.com.ua>
2025-06-14 22:41:58 +0300

    update modules, fix eslint config, add sea test GA

--- eslint.config.js
+++ eslint.config.js
@@ -2,22 +2,26 @@ import globals from 'globals';
 import pluginJs from '@eslint/js';
 
 export default [
-  pluginJs.configs.recommended,
+  {
+    ignores: ['_build/*'],
+  },
   {
     files: ["src/**/*.js"],
     languageOptions: {
-        // ecmaVersion: 2022,
-        // sourceType: 'module',
+        ecmaVersion: 2024,
+        sourceType: 'module',
         globals: {
           ...globals.node,
         },
     },
     rules: {
+      ...pluginJs.configs.recommended.rules,
       "quotes": ["error", "single", { "avoidEscape": true }],
       "semi":   ["error", "always", { "omitLastInOneLineBlock": true}],
       "no-console": "off",
       "no-trailing-spaces": ["error"],
-      "indent": ["error", 4, { "SwitchCase": 1 }]
+      "indent": ["error", 4, { "SwitchCase": 1 }],
+      "no-unused-vars": "warn",
     }
   }
 ];
