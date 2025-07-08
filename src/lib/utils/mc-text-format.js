03281b7a27a63a85c9fa01884314ebd9d920a0b3
Alexander Tsybulsky <atsybulsky@sbcg.com.ua>
2025-07-08 20:14:30 +0300

    move bundlers to streams

--- src/lib/utils/mc-text-format.js
+++ src/lib/utils/mc-text-format.js
@@ -1,95 +1,94 @@
-import { createWriteStream, readFileSync } from 'node:fs';
-import { join } from 'node:path';
-
 import { PassThrough } from 'node:stream';
 import archiver from 'archiver';
 
-export function buildTextBundle(rootDir, fileList, destPath) {
-    const bundler = new TextBundler(rootDir, fileList, destPath);
-    return bundler.bundle();
+export async function buildTextBundle(itemGenerator, ostr) {
+    const bundler = new TextBundler(ostr);
+
+    for (const { name, readStream } of itemGenerator()) {
+        await bundler.append(name, readStream);
+    }
+
+    bundler.end();
 }
 
 const DEFAULT_TEXT_BUNDLE_NAME = 'bundle.txt';
 
-export function buildTextZipBundle(rootDir, fileList, destPath) {
-    const bundler = new TextBundler(rootDir, fileList, destPath);
-    const passThroughStream = new PassThrough();
+export async function buildTextZipBundle(itemGenerator, ostr) {
     const archive = archiver('zip', { zlib: { level: 9 } });
-    const ostr = createWriteStream(destPath);
+    archive.on('warning', (err) => { throw Object.assign(err, { _loc: 'buildTextZipBundle warning' }) });
+    archive.on('error', (err) => { throw Object.assign(err, { _loc: 'buildTextZipBundle error' }) });
+    archive.pipe(ostr);
 
-    return new Promise((resolve, reject) => {
-
-        ostr.on('close', () => ostr.errored ? reject(ostr.errored) : resolve(ostr.bytesWritten));
-        ostr.on('error', reject);
-
-        archive.on('warning', err => reject(Object.assign(err, { _loc: 'buildTextZipBundle warning' })));
-        archive.on('error', err => reject(Object.assign(err, { _loc: 'buildTextZipBundle error' })));
-        // archive.on('close', err => console.log('archive close', err)  );
-        archive.pipe(ostr);
-        archive.append(passThroughStream, { name: DEFAULT_TEXT_BUNDLE_NAME });
-
-        bundler.bundle(passThroughStream).then(textBundleSize => {
+    let textBundleSize = 0;
+    const passThroughStream = new PassThrough();
+    passThroughStream.on('data', chunk => { textBundleSize += chunk.length });
+    passThroughStream.on('end', () => {
+        if (passThroughStream.errored) {
+            console.error(passThroughStream.errored);
+            // ostr.destroy(error); ?
+        } else {
             console.log('Uncompressed text bundle size:', textBundleSize);
-            archive.finalize(); // then?
-        });
-
+            archive.finalize();
+        }
     });
+
+    archive.append(passThroughStream, { name: DEFAULT_TEXT_BUNDLE_NAME });
+    await buildTextBundle(itemGenerator, passThroughStream);
 }
 
 class TextBundler {
-    #rootDir;
-    #destPath;
-    #fileList;
+    #ostr;
+    #fileCount = 0;
 
-    constructor(rootDir, fileList, destPath) {
-        this.#rootDir  = rootDir;
-        this.#destPath = destPath;
-        this.#fileList = fileList.toSorted();
-    }
-
-    bundle(writeHere) {
-        return new Promise((resolve, reject) => {
-            const ostr = writeHere || createWriteStream(this.#destPath);
-            let bytesWrittenCounter = 0;
-            if (ostr.bytesWritten === undefined) { // For streams without bytesWritten prop
-                ostr.on('data', (chunk) => { bytesWrittenCounter += chunk.length });
-            }
-            ostr.on('close', () => ostr.errored
-                ? reject(ostr.errored)
-                : resolve(ostr.bytesWritten ?? bytesWrittenCounter));
-            ostr.on('error', reject);
-
-            try {
-                this.#render(ostr);
-                ostr.end();
-            } catch (error) {
-                ostr.destroy(error);
-            }
-        });
-    }
-    #render(ostr) {
-        ostr.write('!!MOCKUP-LOADER-FORMAT 1.0\n');
-        for (const name of this.#fileList) {
-            this.#renderOneFile(ostr, name);
+    constructor(ostr) {
+        if (!ostr || typeof ostr.write !== 'function' || typeof ostr.end !== 'function') {
+            throw new Error('Invalid output stream provided');
         }
-        ostr.write('\n');
-        ostr.write(`!!FILE-COUNT ${this.#fileList.length}`);
-        // ostr.write('!!EOF\n');
+        this.#ostr = ostr;
     }
-    #renderOneFile(ostr, name) {
-        const filePath = join(this.#rootDir, name);
-        const data = readFileSync(filePath, 'utf-8'); // suppose it's a text file
-        const lines = data.split('\n');
+
+    async append(name, readStream) {
+        if (!name || typeof name !== 'string') throw new Error('Invalid file name provided');
+        if (!readStream || typeof readStream.on !== 'function') throw new Error('Invalid read stream provided');
+
+        if (this.#fileCount === 0) this.#writeHeader();
+
+        await this.#renderOneFile(name, readStream);
+        this.#fileCount++;
+    }
+    end() {
+        this.#writeFooter();
+        this.#ostr.end();
+    }
+
+    #writeHeader() {
+        this.#ostr.write('!!MOCKUP-LOADER-FORMAT 1.0\n');
+    }
+    #writeFooter() {
+        this.#ostr.write('\n');
+        this.#ostr.write(`!!FILE-COUNT ${this.#fileCount}`);
+    }
+    async #renderOneFile(name, readStream) {
+        let data = '';
+
+        readStream.setEncoding('utf-8');
+        await new Promise((resolve, reject) => {
+            readStream.on('data', chunk => { data += chunk });
+            readStream.on('end', resolve);
+            readStream.on('error', reject);
+            readStream.resume();
+        });
 
         // trim trailing empty lines
+        const lines = data.split('\n');
         while (lines.length > 0 && lines[lines.length - 1].trim() === '') {
             lines.pop();
         }
         const dataLines = lines.length;
         lines.push(''); // add a blank line at the end
 
-        ostr.write('\n');
-        ostr.write(`!!FILE ${name} text ${dataLines}\n`);
-        ostr.write(lines.join('\n'));
+        this.#ostr.write('\n');
+        this.#ostr.write(`!!FILE ${name} text ${dataLines}\n`);
+        this.#ostr.write(lines.join('\n'));
     }
 }
--- src/lib/utils/zip.js
+++ src/lib/utils/zip.js
@@ -1,23 +1,18 @@
-import { createWriteStream } from 'node:fs';
-import path from 'node:path';
 import archiver from 'archiver';
 
-export function zipFiles(rootDir, fileList, zipPath) {
-    return new Promise((resolve, reject) => {
-        const archive = archiver('zip', { zlib: { level: 9 } });
+export async function zipFiles(itemGenerator, ostr) {
+    const archive = archiver('zip', { zlib: { level: 9 } });
+    archive.on('warning', (err) => { throw Object.assign(err, { _loc: 'zipFiles warning' }) });
+    archive.on('error', (err) => { throw Object.assign(err, { _loc: 'zipFiles error' }) });
+    archive.pipe(ostr);
 
-        const ostr = createWriteStream(zipPath);
-        ostr.on('close', () => ostr.errored ? reject(ostr.errored) : resolve(ostr.bytesWritten)); // archive.pointer()
-        ostr.on('error', reject);
+    for (const { name, readStream } of itemGenerator()) {
+        await new Promise((resolve, reject) => {
+            readStream.on('end', resolve);
+            readStream.on('error', reject);
+            archive.append(readStream, { name });
+        });
+    }
 
-        archive.on('warning', (err) => reject(Object.assign(err, { _loc: 'zipFiles warning' })));
-        archive.on('error', (err) => reject(Object.assign(err, { _loc: 'zipFiles error' })));
-        archive.pipe(ostr);
-
-        for (let name of fileList) {
-            archive.file(path.join(rootDir, name), { name });
-        }
-
-        archive.finalize();
-    });
+    await archive.finalize();
 }
